b08e2af58758aae6aa1f0d63e443eb70e199463a
T3tje <tilman2013@gmail.com>
2023-12-06 17:08:56 +0100

    add css Votebutton and props to articlekomponent

--- src/Article1.tsx
+++ src/Article1.tsx
@@ -1,10 +1,30 @@
+import {useState} from "react";
 
-export default function Article1() {
+
+type articleProps = {
+    h2: string,
+    p1: string,
+    p2:string,
+}
+
+export default function Article1(props:articleProps) {
+    const [count, setCount] = useState<number>(0)
+
+    const increaseCount = () => {
+        setCount(count +1)
+    }
+
     return (
         <article id="eins">
-            <h2>Optimizing Your Car Sound System</h2>
-            <p>Explore tips and tricks to enhance your car audio experience.</p>
-            <p className="rounded-tag">Tag: Car Audio</p>
+            <h2 >{props.h2}</h2>
+            <p>{props.p1}</p>
+            <p className="rounded-tag">{props.p2}</p>
+            <div className={"vote"}>
+                <p className="countNumber">{count}</p>
+                <button onClick={increaseCount}>Vote</button>
+            </div>
+
+
         </article>
     )
 }
--- src/Blog.tsx
+++ src/Blog.tsx
@@ -1,20 +1,24 @@
 import Article1 from "./Article1.tsx";
-import Article2 from "./Article2.tsx";
-import Article3 from "./Article3.tsx";
 import Nav from "./Nav.tsx";
 import Sidebar from "./Sidebar.tsx";
 import Footer from "./Footer.tsx";
 import Header from "./Header.tsx";
+import {useState} from "react";
+
+
 export default function Blog() {
+
+
+
    return (
     <>
         <Header/>
 
         <Nav/>
+        <Article1 h2={"Optimizing Your Car Sound System"} p1={"Explore tips and tricks to enhance your car audio experience."} p2={"Tag: Car Audio"}/>
+        <Article1 h2={"Choosing the Right Car Stereo"} p1={"Guidance on selecting the perfect car stereo for your needs."} p2={"Tag: Audio Equipment"}/>
+        <Article1 h2={"DIY Car Speaker Installations"} p1={"Step-by-step instructions for installing speakers in your vehicle."} p2={"Tag: Installation"}/>
 
-        <Article1/>
-        <Article2/>
-        <Article3/>
 
         <Sidebar/>
 
